20611538edc15ce056d68e39d5c30b5ae9894d86
zhan3 <2375515459@qq.com>
2026-04-26 10:05:30 +0800

    开发环境baseurl修改, 修复登录后不自动跳转home

--- utils/env.js
+++ utils/env.js
@@ -4,8 +4,8 @@ const { envVersion } = miniProgram
 let env = { baseURL: 'http://localhost:8080' }
 switch (envVersion) {
   case 'develop': env.baseURL = 'http://localhost:8080'; break;
-  case 'trial':   env.baseURL = 'http://localhost:8080'; break;
-  case 'release': env.baseURL = 'http://localhost:8080'; break;
+  case 'trial':   env.baseURL = 'http://120.46.84.175:8080'; break;
+  case 'release': env.baseURL = 'http://120.46.84.175:8080'; break;
   default:       env.baseURL = 'http://localhost:8080'; break;
 }
 
