9d230e0c560a33aefd81023693770c706fe7009b
zhan3 <2375515459@qq.com>
2026-09-05 11:50:36 +0800

    fix: separate miniprogram API URLs by environment

--- utils/env.js
+++ utils/env.js
@@ -1,25 +1,28 @@
 const { miniProgram } = wx.getAccountInfoSync()
 const { envVersion } = miniProgram
 
-let env = {
-  baseURL: 'http://localhost:8080'
+// 微信开发者工具中的开发版可以访问本机；真机调试时请改为同一局域网内可访问的地址。
+// 体验版和正式版必须使用已在微信公众平台登记的 HTTPS 合法域名。
+const API_BASE_URLS = Object.freeze({
+  develop: 'http://localhost:8080',
+  trial: '',
+  release: ''
+})
+
+function resolveBaseURL(version) {
+  const baseURL = API_BASE_URLS[version]
+  if (!baseURL) {
+    throw new Error(`未配置 ${version} 环境的后端 API 地址`)
+  }
+  if (version !== 'develop' && !baseURL.startsWith('https://')) {
+    throw new Error(`${version} 环境的后端 API 地址必须使用 HTTPS`)
+  }
+  return baseURL.replace(/\/$/, '')
 }
-switch (envVersion) {
-  // 开发版
-  case 'develop':
-    env.baseURL = 'http://localhost:8080'
-    break;
-  // 体验�?
-  case 'trial':
-    env.baseURL = 'http://localhost:8080'
-    break;
-  // 正式�?
-  case 'release':
-    env.baseURL = 'http://localhost:8080'
-    break;
-  default:
-    env.baseURL = 'http://localhost:8080'
-    break;
-}
+
+const env = Object.freeze({
+  version: envVersion,
+  baseURL: resolveBaseURL(envVersion)
+})
 
 export { env }
